78b6c7f1ab7d5eb4e3522f023903ac1fccb17502
JoeWangu <joewangu2021@outlook.com>
2024-08-18 13:07:39 +0300

    first commit

---
+++ netapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+# import requests
+
+# headers = {
+#   'Content-Type': 'application/json',
+#   'Authorization': 'Bearer Lsrfcm6zV9kHGDDNUhYWUCh5Dnny'
+# }
+
+# payload = {
+#     "ShortCode": 600980,
+#     "CommandID": "CustomerBuyGoodsOnline",
+#     "amount": "1",
+#     "MSISDN": "254705912645",
+#     "BillRefNumber": "",
+#   }
+
+
+# response = requests.request("POST", 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate', headers = headers, data = payload)
+
+# print(response.text.encode('utf8'))
